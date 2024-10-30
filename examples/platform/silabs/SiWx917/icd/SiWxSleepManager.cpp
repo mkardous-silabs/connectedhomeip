76025b383949e5b431ebc94bcb247abc71242b6b
Mathieu Kardous <mathieu.kardous@silabs.com>
2024-10-30 16:32:29 -0400

    Class creation and build setup

---
+++ examples/platform/silabs/SiWx917/icd/SiWxSleepManager.cpp
@@ -0,0 +1,48 @@
+/*
+ *    Copyright (c) 2024 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include "SiWxSleepManager.h"
+
+using namespace chip::app;
+
+// Initialize the static instance
+SiWxSleepManager SiWxSleepManager::mInstance;
+
+CHIP_ERROR SiWxSleepManager::Init()
+{
+    // Initialization logic
+    return CHIP_NO_ERROR;
+}
+
+void SiWxSleepManager::OnEnterActiveMode()
+{
+    // Execution logic for entering active mode
+}
+
+void SiWxSleepManager::OnEnterIdleMode()
+{
+    // Execution logic for entering idle mode
+}
+
+void SiWxSleepManager::OnSubscriptionEstablished(ReadHandler & aReadHandler)
+{
+    // Implement logic for when a subscription is established
+}
+
+void SiWxSleepManager::OnSubscriptionTerminated(ReadHandler & aReadHandler)
+{
+    // Implement logic for when a subscription is terminated
+}
---
+++ examples/platform/silabs/SiWx917/icd/SiWxSleepManager.h
@@ -0,0 +1,68 @@
+/*
+ *    Copyright (c) 2024 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+#pragma once
+
+#include <app/ReadHandler.h>
+#include <app/icd/server/ICDManager.h>
+#include <app/icd/server/ICDStateObserver.h>
+#include <lib/core/CHIPError.h>
+
+/**
+ * @brief SiWxSleepManager is a singleton class that manages the sleep modes of the SiWx917 device.
+ *        The class contains the buisness logic associated with optimizing the sleep states of the SiWx Platform based on the Matter
+ *        SDK internal states
+ *
+ *        The class implements two disctint optimization states; one of SIT devices and one of LIT devices
+ *        For SIT ICDs, the logic is based on the Subscriptions established with the device.
+ *        For LIT ICDs, the logic is based on the ICDManager operating modes.
+ */
+class SiWxSleepManager : public chip::app::ICDStateObserver, public chip::app::ReadHandler::ApplicationCallback
+{
+public:
+    SiWxSleepManager(const SiWxSleepManager &)             = delete;
+    SiWxSleepManager & operator=(const SiWxSleepManager &) = delete;
+
+    static SiWxSleepManager & GetInstance() { return mInstance; }
+
+    /**
+     * @brief Init function that configure the SiWxSleepManager APIs based on the type of ICD
+     *        SIT ICD: Init function registers the ReadHandler Application callback to be notified when a subscription is
+     *                 established or destroyed.
+     *
+     *        LIT ICD: Init function registers with the ICDManager as an observer to be notified of the ICD mode changes.
+     *
+     * @return CHIP_ERROR
+     */
+    CHIP_ERROR Init();
+
+    // ICDSateObserver implementation overrides
+
+    void OnEnterActiveMode() override;
+    void OnEnterIdleMode() override;
+    void OnTransitionToIdle() override { /* No execution logic */ }
+    void OnICDModeChange() override { /* No execution logic */ }
+
+    // ReadHandler::ApplicationCallback implementation overrides
+
+    void OnSubscriptionEstablished(chip::app::ReadHandler & aReadHandler);
+    void OnSubscriptionTerminated(chip::app::ReadHandler & aReadHandler);
+
+private:
+    SiWxSleepManager()  = default;
+    ~SiWxSleepManager() = default;
+
+    static SiWxSleepManager mInstance;
+};
